6140a9da432dec89a6a797b9c021cdab1dcb515c
arfan21 <arfan.jadul10@gmail.com>
2021-03-11 16:20:38 +0700

    fix indexing

--- CREATE_INDEX.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX price_index ON PRODUCTS(price);
-CREATE INDEX price_currency_index ON PRODUCTS(price_currency);
-CREATE INDEX name_index ON CUSTOMERS(name);
-CREATE INDEX created_at_index ON CARTS(created_at);
--- INDEXING.sql
+++ INDEXING.sql
@@ -1,12 +1,9 @@
-ALTER TABLE PRODUCTS(
-    CREATE INDEX price_index ON PRODUCTS(price);
-    CREATE INDEX price_currency_index ON PRODUCTS(price_currency);
-)
+-- add index to table PRODUCTS 
+CREATE INDEX price_index ON PRODUCTS(price);
+CREATE INDEX price_currency_index ON PRODUCTS(price_currency);
 
-ALTER TABLE CUSTOMERS(
-    CREATE INDEX name_index ON CUSTOMERS(name);
-)
+-- add index to table CUSTOMERS
+CREATE INDEX name_index ON CUSTOMERS(name);
 
-ALTER TABLE CARTS(
-    CREATE INDEX created_at_index ON CARTS(created_at);
-)
+-- add index to table CARTS
+CREATE INDEX created_at_index ON CARTS(created_at);
